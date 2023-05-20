1a07d04198d1a62a5ccd74f181d850df3a743780
AliSina Karimi <100498538+AliSinaDevelo@users.noreply.github.com>
2023-05-20 02:49:38 +0200

    server boiler plate added

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const MongoClient = require('mongodb').MongoClient;
+const url = 'mongodb://localhost:27017';
+const dbName = 'portfolio';
+
+MongoClient.connect(url, { useUnifiedTopology: true }, (err, client) => {
+  if (err) return console.error(err);
+  console.log('Connected to MongoDB');
+
+  const db = client.db(dbName);
+
+  // Define your routes and API endpoints here
+
+  app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
+});
